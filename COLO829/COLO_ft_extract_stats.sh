7bd65f4f8aa59daeb2f5519943be7d696e98679e
Elliott Swanson <30820996+swansoe@users.noreply.github.com>
2025-04-15 16:59:00 -0700

    Delete COLO829 directory

--- COLO829/COLO_ft_extract_stats.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# conda activate fibertools
-
-ft ddda-to-m6a hap2_T_only.bam | ft add-nucleosomes - hap2_T_only_nuc.bam
-samtools index hap2_T_only_nuc.bam
-ft extract hap2_T_only_nuc.bam --all hap2_T_only_ft_extract_all.bed
