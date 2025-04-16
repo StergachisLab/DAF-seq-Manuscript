7f6292806bd0c8198f40dd712b6788fa242ff3e3
Elliott Swanson <30820996+swansoe@users.noreply.github.com>
2025-04-16 10:16:34 -0700

    Update 04_NAPA_da_prop_phased.py

--- Targeted/NAPA/04_NAPA_da_prop_phased.py
+++ Targeted/NAPA/04_NAPA_da_prop_phased.py
@@ -120,16 +120,3 @@ with open(out_bg,'w') as fout:
         if v['da'] > 0: # filter 0 positions
             writer.writerow([reg_chrom, k, k+1, v['da']/v['total']])
 
-
-# sort -k1,1 -k2,2n NAPA_PS00626_DA_density.bg > temp.bg
-# mv temp.bg NAPA_PS00626_DA_density.bg
-# /gscratch/stergachislab/install_dir/bedGraphToBigWig NAPA_PS00626_DA_density.bg /mmfs1/gscratch/stergachislab/assemblies/hg38.analysisSet.chrom.sizes NAPA_PS00626_DA_density.bw
-
-# sort -k1,1 -k2,2n NAPA_PS00626_Hap1_DA_density.bg > temp.bg
-# mv temp.bg NAPA_PS00626_Hap1_DA_density.bg
-# /gscratch/stergachislab/install_dir/bedGraphToBigWig NAPA_PS00626_Hap1_DA_density.bg /mmfs1/gscratch/stergachislab/assemblies/hg38.analysisSet.chrom.sizes NAPA_PS00626_Hap1_DA_density.bw
-
-# sort -k1,1 -k2,2n NAPA_PS00626_Hap2_DA_density.bg > temp.bg
-# mv temp.bg NAPA_PS00626_Hap2_DA_density.bg
-# /gscratch/stergachislab/install_dir/bedGraphToBigWig NAPA_PS00626_Hap2_DA_density.bg /mmfs1/gscratch/stergachislab/assemblies/hg38.analysisSet.chrom.sizes NAPA_PS00626_Hap2_DA_density.bw
-
