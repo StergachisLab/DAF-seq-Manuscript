fc40f5104b985765bd4e2d3659b47e0db74cedf1
Elliott Swanson <30820996+swansoe@users.noreply.github.com>
2025-04-17 14:40:40 -0700

    Update and rename 01_prepare_ft_footprint_UBA1.py to 02_prepare_ft_footprint_UBA1.py

--- Targeted/UBA1/footprinting/02_prepare_ft_footprint_UBA1.py
+++ Targeted/UBA1/footprinting/02_prepare_ft_footprint_UBA1.py
@@ -3,8 +3,6 @@ import subprocess
 from glob import glob
 import os
 
-# cat fimo_out/fimo.tsv | awk '$9 <= 0.05' | grep MA > motifs/filtered_fimo.tsv
-# cat motifs/filtered_fimo.tsv | awk '{OFS="\t"}{ print $3,$4,$5,$2,$7,$6 }' > motifs/filtered_fimo.bed
 
 motif_bed = 'motifs/filtered_fimo.bed'
 
@@ -30,8 +28,8 @@ for k,v in motifs.items():
         yaml.write(f'  - [0, {motif_length}]\n')
 
 
-H1_BAM = '/mmfs1/gscratch/stergachislab/swansoe/projects/DddA/PCR_Dev/UBA1/pileups/GM12878_UBA1_PS00685_nuc_H1.bam'
-H2_BAM = '/mmfs1/gscratch/stergachislab/swansoe/projects/DddA/PCR_Dev/UBA1/pileups/GM12878_UBA1_PS00685_nuc_H2.bam'
+H1_BAM = '../pileups/GM12878_UBA1_PS00685_nuc_H1.bam'
+H2_BAM = '../pileups/GM12878_UBA1_PS00685_nuc_H2.bam'
 
 # run ft footprint
 name = 'H1_GM12878_UBA1_PS00685'
@@ -48,21 +46,6 @@ for k in motifs.keys():
     subprocess.run(shell_command, shell=True, check=True, capture_output=True, text=True)
 
 
-
 # manually ran ft footprint for CTCF using individual modules
-# /mmfs1/gscratch/stergachislab/bin/ft footprint -x "len(msp)>150" -t 30 --bed motifs/canonical_CTCF_motif.bed --yaml modules/ctcf_modules.yaml $H1_BAM > ft_out/ft_H1_ModCTCF.bed
-# /mmfs1/gscratch/stergachislab/bin/ft footprint -x "len(msp)>150" -t 30 --bed motifs/canonical_CTCF_motif.bed --yaml modules/ctcf_modules.yaml $H2_BAM > ft_out/ft_H2_ModCTCF.bed
-
-# /mmfs1/gscratch/stergachislab/bin/ft footprint -x "len(msp)>150" -t 30 --bed motifs/ctcf2_motif.bed --yaml modules/ctcf_modules.yaml $H1_BAM > ft_out/ft_H1_ctcf2_ModCTCF.bed
-# /mmfs1/gscratch/stergachislab/bin/ft footprint -x "len(msp)>150" -t 30 --bed motifs/ctcf2_motif.bed --yaml modules/ctcf_modules.yaml $H2_BAM > ft_out/ft_H2_ctcf2_ModCTCF.bed
-
-
-# In [7]: mod2
-# Out[7]: 'CGACACA'
-
-# In [8]: mod3
-# Out[8]: 'AGATGG'
-
-# In [9]: sequence[16:29]
-# Out[9]: 'CGACACAAGATGG'
-
+ft footprint -x "len(msp)>150" -t 30 --bed motifs/canonical_CTCF_motif.bed --yaml modules/ctcf_modules.yaml $H1_BAM > ft_out/ft_H1_ModCTCF.bed
+ft footprint -x "len(msp)>150" -t 30 --bed motifs/canonical_CTCF_motif.bed --yaml modules/ctcf_modules.yaml $H2_BAM > ft_out/ft_H2_ModCTCF.bed
