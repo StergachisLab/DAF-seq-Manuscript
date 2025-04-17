b3066f6a6a88bc7a26f5aa0bf58e880a8490ba51
Elliott Swanson <30820996+swansoe@users.noreply.github.com>
2025-04-17 11:14:10 -0700

    Update 03_make_NAPA_pileups.sh

--- Targeted/NAPA/qc_plots/03_make_NAPA_pileups.sh
+++ Targeted/NAPA/qc_plots/03_make_NAPA_pileups.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-bedtools intersect -a nuc_positions.bed -b ../napa_region.bed | bedtools genomecov -bg -i stdin -g /gscratch/stergachislab/assemblies/hg38.analysisSet.chrom.sizes > nuc_positions.bg
-bedtools intersect -a small_msp_positions.bed -b ../napa_region.bed | bedtools genomecov -bg -i stdin -g /gscratch/stergachislab/assemblies/hg38.analysisSet.chrom.sizes > small_msp_positions.bg
-bedtools intersect -a large_msp_positions.bed -b ../napa_region.bed | bedtools genomecov -bg -i stdin -g /gscratch/stergachislab/assemblies/hg38.analysisSet.chrom.sizes > large_msp_positions.bg
-/gscratch/stergachislab/install_dir/bedGraphToBigWig nuc_positions.bg /mmfs1/gscratch/stergachislab/assemblies/hg38.analysisSet.chrom.sizes nuc_positions.bw
-/gscratch/stergachislab/install_dir/bedGraphToBigWig small_msp_positions.bg /mmfs1/gscratch/stergachislab/assemblies/hg38.analysisSet.chrom.sizes small_msp_positions.bw
-/gscratch/stergachislab/install_dir/bedGraphToBigWig large_msp_positions.bg /mmfs1/gscratch/stergachislab/assemblies/hg38.analysisSet.chrom.sizes large_msp_positions.bw
+bedtools intersect -a nuc_positions.bed -b ../napa_region.bed | bedtools genomecov -bg -i stdin -g ../hg38.analysisSet.chrom.sizes > nuc_positions.bg
+bedtools intersect -a small_msp_positions.bed -b ../napa_region.bed | bedtools genomecov -bg -i stdin -g ../hg38.analysisSet.chrom.sizes > small_msp_positions.bg
+bedtools intersect -a large_msp_positions.bed -b ../napa_region.bed | bedtools genomecov -bg -i stdin -g ../hg38.analysisSet.chrom.sizes > large_msp_positions.bg
+bedGraphToBigWig nuc_positions.bg ../hg38.analysisSet.chrom.sizes nuc_positions.bw
+bedGraphToBigWig small_msp_positions.bg ../hg38.analysisSet.chrom.sizes small_msp_positions.bw
+bedGraphToBigWig large_msp_positions.bg ../hg38.analysisSet.chrom.sizes large_msp_positions.bw
