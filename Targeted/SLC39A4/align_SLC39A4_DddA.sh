ec4c543c201888270eb30fcd502ac3e2dd5c9b2e
Elliott Swanson <30820996+swansoe@users.noreply.github.com>
2025-04-17 11:37:45 -0700

    Update align_SLC39A4_DddA.sh

--- Targeted/SLC39A4/align_SLC39A4_DddA.sh
+++ Targeted/SLC39A4/align_SLC39A4_DddA.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-FASTA="/mmfs1/gscratch/stergachislab/assemblies/simple-names/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
+FASTA="../data/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
 
-BAMS=('Colon_SLC39A4_PS00682_m84046_240802_231812_s1.hifi_reads.bc2086.bam' 'GM12878_SLC39A4_PS00686_m84046_240802_231812_s1.hifi_reads.bc2094.bam' 'Heart_SLC39A4_PS00681_m84046_240802_231812_s1.hifi_reads.bc2085.bam' 'Liver_SLC39A4_PS00680_m84046_240802_231812_s1.hifi_reads.bc2084.bam')
+BAMS=('../data/Colon_SLC39A4_PS00682_m84046_240802_231812_s1.hifi_reads.bc2086.bam' '../data/GM12878_SLC39A4_PS00686_m84046_240802_231812_s1.hifi_reads.bc2094.bam' '../data/Heart_SLC39A4_PS00681_m84046_240802_231812_s1.hifi_reads.bc2085.bam' '../data/Liver_SLC39A4_PS00680_m84046_240802_231812_s1.hifi_reads.bc2084.bam')
 
 for b in "${BAMS[@]}"; do 
 minimap2 -t 40 --MD -Y -y -a -x map-pb $FASTA <(samtools fastq -T "*" "$b") | samtools sort > ${b/bam/map-pb.bam}
@@ -11,10 +11,10 @@ done
 
 # replace Da with IUPAC codes
 for b in "${BAMS[@]}"; do 
-python /mmfs1/gscratch/stergachislab/swansoe/projects/DddA/process_DddA_bam.py -b ${b/bam/map-pb.bam};
+python ../../General/process_DddA_bam.py -b ${b/bam/map-pb.bam};
 done
 
-COR_BAMS=('Colon_SLC39A4_PS00682_m84046_240802_231812_s1.hifi_reads.bc2086.map-pb_corrected.bam' 'GM12878_SLC39A4_PS00686_m84046_240802_231812_s1.hifi_reads.bc2094.map-pb_corrected.bam' 'Heart_SLC39A4_PS00681_m84046_240802_231812_s1.hifi_reads.bc2085.map-pb_corrected.bam' 'Liver_SLC39A4_PS00680_m84046_240802_231812_s1.hifi_reads.bc2084.map-pb_corrected.bam')
+COR_BAMS=('../data/Colon_SLC39A4_PS00682_m84046_240802_231812_s1.hifi_reads.bc2086.map-pb_corrected.bam' '../data/GM12878_SLC39A4_PS00686_m84046_240802_231812_s1.hifi_reads.bc2094.map-pb_corrected.bam' '../data/Heart_SLC39A4_PS00681_m84046_240802_231812_s1.hifi_reads.bc2085.map-pb_corrected.bam' '../data/Liver_SLC39A4_PS00680_m84046_240802_231812_s1.hifi_reads.bc2084.map-pb_corrected.bam')
 
 for b in "${COR_BAMS[@]}"; do
 samtools index $b;
