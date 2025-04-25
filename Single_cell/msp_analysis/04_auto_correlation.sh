2dcf841a03bf87e4c3f2bae022640f7631ccb73f
Elliott Swanson <30820996+swansoe@users.noreply.github.com>
2025-04-25 13:58:02 -0700

    Update 04_auto_correlation.sh

--- Single_cell/msp_analysis/04_auto_correlation.sh
+++ Single_cell/msp_analysis/04_auto_correlation.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 samples=('PS00718' 'PS00756' 'PS00757' 'PS00758' 'PS00867' 'PS00868' 'PS00869' 'PS00870' 'PS00871' 'PS00872' 'PS00873' 'PS00874')
 
 for s in "${samples[@]}"; do
-    /mmfs1/gscratch/stergachislab/bin/ft qc "fibertools_msp/${s}_consensus_BothStrands_HG38_corrected.haplotagged.m6A_nuc.bam" --acf > "${s}_ft_qc.tsv"
+    ft qc "fibertools_msp/${s}_consensus_BothStrands_HG38_corrected.haplotagged.m6A_nuc.bam" --acf > "${s}_ft_qc.tsv"
 done
 
 for s in "${samples[@]}"; do
