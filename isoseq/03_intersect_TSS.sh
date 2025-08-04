d1ad9d62808135275b4c503847c692805b9dcd50
Elliott Swanson <30820996+swansoe@users.noreply.github.com>
2025-08-04 12:06:24 -0700

    Update 03_intersect_TSS.sh

--- isoseq/03_intersect_TSS.sh
+++ isoseq/03_intersect_TSS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-HG_PEAKS="/mmfs1/gscratch/stergachislab/swansoe/projects/DddA/single_cell/expt1_HG002_FACS/msp_analysis/FIRE-peaks_HG002_noUnreliableCoverage.AutosomesOnly.bed.gz"
+HG_PEAKS="../msp_analysis/FIRE-peaks_HG002_noUnreliableCoverage.AutosomesOnly.bed.gz"
 bedmap --ec --echo --echo-map-id --skip-unmapped <(zcat $HG_PEAKS | sort-bed -) <( sort-bed isoseq_counts_giab_hg002.bed ) | sort-bed - | bgzip -@ 10 > isoseq_counts_giab_hg002_Peaks_bedmap.bed.gz
 
 
