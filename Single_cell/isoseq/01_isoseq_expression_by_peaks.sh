0f44ab9454a8b8cb4e7c901d5547d07ec9a86088
Elliott Swanson <30820996+swansoe@users.noreply.github.com>
2025-08-04 12:08:48 -0700

    Update 01_isoseq_expression_by_peaks.sh

--- Single_cell/isoseq/01_isoseq_expression_by_peaks.sh
+++ Single_cell/isoseq/01_isoseq_expression_by_peaks.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # conda activate isoseq
 
-pbmm2 align --preset ISOSEQ --sort giab_na24385.hifi_reads.lima.0--0.lima.IsoSeqX_bc02_5p--IsoSeqX_3p.refined.bam /gscratch/stergachislab/assemblies/simple-names/hg38.fa giab_hg002_isoseq_mapped.bam
+pbmm2 align --preset ISOSEQ --sort giab_na24385.hifi_reads.lima.0--0.lima.IsoSeqX_bc02_5p--IsoSeqX_3p.refined.bam hg38.fa giab_hg002_isoseq_mapped.bam
 
 COLLAPSED="giab_hg002_isoseq_mapped.collapsed.gff"
 isoseq collapse --do-not-collapse-extra-5exons giab_hg002_isoseq_mapped.bam $COLLAPSED
