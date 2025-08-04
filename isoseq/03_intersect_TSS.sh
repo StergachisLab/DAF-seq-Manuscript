c4582de6172544bc07caf9c22c36b6b6e0ec4797
Elliott Swanson <30820996+swansoe@users.noreply.github.com>
2025-08-04 12:07:48 -0700

    Delete isoseq directory

--- isoseq/03_intersect_TSS.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-HG_PEAKS="../msp_analysis/FIRE-peaks_HG002_noUnreliableCoverage.AutosomesOnly.bed.gz"
-bedmap --ec --echo --echo-map-id --skip-unmapped <(zcat $HG_PEAKS | sort-bed -) <( sort-bed isoseq_counts_giab_hg002.bed ) | sort-bed - | bgzip -@ 10 > isoseq_counts_giab_hg002_Peaks_bedmap.bed.gz
-
-
