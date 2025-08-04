53e31e7950abd47e5818f9d8f48d1a76d21b1f53
Elliott Swanson <30820996+swansoe@users.noreply.github.com>
2025-08-04 12:28:38 -0700

    Add TSS enrichment analyses during revision

--- Single_cell/scDAF_figures/Genomic_Enrichments/05_intersect_MSPs.sh
+++ Single_cell/scDAF_figures/Genomic_Enrichments/05_intersect_MSPs.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 
-GENCODE=/mmfs1/gscratch/stergachislab/swansoe/projects/DddA/single_cell/expt1_HG002_FACS/msp_analysis/gencodev45_Ensembl_canonical_TSS.bed
-FT=/mmfs1/gscratch/stergachislab/bin/ft
-SIZES=/mmfs1/gscratch/stergachislab/assemblies/hg38.analysisSet.chrom.sizes
-REF=/mmfs1/gscratch/stergachislab/swansoe/ref_genomes/hg38_bt2/hg38.analysisSet
+GENCODE=../../msp_analysis/gencodev45_Ensembl_canonical_TSS.bed
+FT=ft
+SIZES=../../msp_analysis/hg38.analysisSet.chrom.sizes
+REF=hg38.analysisSet
 
 
 # Fiber-seq ---------------------------------------------------------
@@ -14,8 +14,8 @@ REF=/mmfs1/gscratch/stergachislab/swansoe/ref_genomes/hg38_bt2/hg38.analysisSet
 
 # HG002
 SNAME="HG002"
-FS_BAM="/mmfs1/gscratch/stergachislab/mvollger/projects/FIREv2.0/results/HG002/fire/HG002.fire.cram"
-UNFILT_FIRE="/mmfs1/gscratch/stergachislab/mvollger/projects/FIREv2.0/results/HG002/FDR-peaks/FDR-FIRE-peaks.bed.gz"
+FS_BAM="../../data/HG002.fire.cram"
+UNFILT_FIRE="../../data/FDR-FIRE-peaks.bed.gz"
 FIRE_PEAKS="tss_enrichment_intermediate/${SNAME}_FIRE_peaks_filtered.bed.gz"
 zcat $UNFILT_FIRE | awk '$NF == "true"' | bgzip -@ 10 > $FIRE_PEAKS
 # only include TSSs that are in FIRE peaks (active TSSs) with reliable coverage as determined in MSP analysis 
@@ -47,8 +47,8 @@ $FT extract -r -t 30 $FS_BAM --all - | sed 1d | datamash sum 14 sum 15 | awk '{O
 
 SAMPLES=("GM12878")
 for SNAME in "${SAMPLES[@]}"; do
-    FS_BAM="/mmfs1/gscratch/stergachislab/FIRE/results/${SNAME}/${SNAME}-fire-v0.1.1-filtered.cram"
-    UNFILT_FIRE="/mmfs1/gscratch/stergachislab/FIRE/results/${SNAME}/${SNAME}-fire-v0.1.1-peaks.bed.gz"
+    FS_BAM="../../data/${SNAME}/${SNAME}-fire-v0.1.1-filtered.cram"
+    UNFILT_FIRE="../../data/${SNAME}/${SNAME}-fire-v0.1.1-peaks.bed.gz"
     FIRE_PEAKS="tss_enrichment_intermediate/${SNAME}_FIRE_peaks_filtered.bed.gz"
     zcat $UNFILT_FIRE | awk '$NF == "true"' | bgzip -@ 10 > $FIRE_PEAKS
     # only include TSSs that are in FIRE peaks (active TSSs) with reliable coverage as determined in MSP analysis 
@@ -82,8 +82,8 @@ done
 # FIRE v0.1 ---------------------------------------------
 SAMPLES=("CHM13")
 for SNAME in "${SAMPLES[@]}"; do
-    FS_BAM="/mmfs1/gscratch/stergachislab/FIRE/results/${SNAME}/${SNAME}-fire-v0.1-filtered.cram"
-    UNFILT_FIRE="/mmfs1/gscratch/stergachislab/FIRE/results/${SNAME}/${SNAME}-fire-v0.1-peaks.bed.gz"
+    FS_BAM="../../data/${SNAME}/${SNAME}-fire-v0.1-filtered.cram"
+    UNFILT_FIRE="../../data/${SNAME}/${SNAME}-fire-v0.1-peaks.bed.gz"
     FIRE_PEAKS="tss_enrichment_intermediate/${SNAME}_FIRE_peaks_filtered.bed.gz"
     zcat $UNFILT_FIRE | awk '$NF == "true"' | bgzip -@ 10 > $FIRE_PEAKS
     # only include TSSs that are in FIRE peaks (active TSSs) with reliable coverage as determined in MSP analysis 
@@ -117,8 +117,8 @@ done
 
 SAMPLES=("PS00272" "PS00321" "PS00327" "PS00381" "PS00382" "PS00383" "PS00384" "ST001-liver" "ST001-lung" "PS00338_COLO829BL_1" "PS00356_COLO829BL_2" "COLO_T_2_PS00_418_451_488")
 for SNAME in "${SAMPLES[@]}"; do
-    FS_BAM="/mmfs1/gscratch/stergachislab/mvollger/projects/FIREv2.0/results/${SNAME}/fire/${SNAME}.fire.cram"
-    UNFILT_FIRE="/mmfs1/gscratch/stergachislab/mvollger/projects/FIREv2.0/results/${SNAME}/FDR-peaks/FDR-FIRE-peaks.bed.gz"
+    FS_BAM="../../data/${SNAME}/fire/${SNAME}.fire.cram"
+    UNFILT_FIRE="../../data/results/${SNAME}/FDR-peaks/FDR-FIRE-peaks.bed.gz"
     FIRE_PEAKS="tss_enrichment_intermediate/${SNAME}_FIRE_peaks_filtered.bed.gz"
     zcat $UNFILT_FIRE | awk '$NF == "true"' | bgzip -@ 10 > $FIRE_PEAKS
     # only include TSSs that are in FIRE peaks (active TSSs) with reliable coverage as determined in MSP analysis 
@@ -151,8 +151,8 @@ done
 
 # Colon descending (SMaHT) ----------------------------------------------------------------------------
 SNAME="PS30743"
-FS_BAM="/mmfs1/gscratch/stergachislab/userprod/production/fibertools-pipeline/PacBio-Fiber-seq/PS30743/hg38/results/pipelines/fire-run/results/PS30743/PS30743-fire-v0.1.2-filtered.cram"
-UNFILT_FIRE="/mmfs1/gscratch/stergachislab/userprod/production/fibertools-pipeline/PacBio-Fiber-seq/PS30743/hg38/results/pipelines/fire-run/results/PS30743/PS30743-fire-v0.1.2-peaks.bed.gz"
+FS_BAM="../../data/PS30743/PS30743-fire-v0.1.2-filtered.cram"
+UNFILT_FIRE="../../data/results/pipelines/fire-run/results/PS30743/PS30743-fire-v0.1.2-peaks.bed.gz"
 FIRE_PEAKS="tss_enrichment_intermediate/${SNAME}_FIRE_peaks_filtered.bed.gz"
 zcat $UNFILT_FIRE | awk '$NF == "true"' | bgzip -@ 10 > $FIRE_PEAKS
 # only include TSSs that are in FIRE peaks (active TSSs) with reliable coverage as determined in MSP analysis 
@@ -196,7 +196,7 @@ done
 # Single-cell DAF-seq ----------------------------------------------------------------------------
 
 # only include TSSs that are in HG002 peaks (active TSSs) with reliable coverage as determined in MSP analysis 
-FIRE_PEAKS="/mmfs1/gscratch/stergachislab/swansoe/projects/DddA/single_cell/expt1_HG002_FACS/msp_analysis/FIRE-peaks_HG002_noUnreliableCoverage.AutosomesOnly.bed.gz"
+FIRE_PEAKS="../../msp_analysis/FIRE-peaks_HG002_noUnreliableCoverage.AutosomesOnly.bed.gz"
 ALL_TSS="gencodev45_Ensembl_canonical_TSS_HG002_FIRE_noUnreliableCoverage.bed"
 bedtools intersect -a $GENCODE -b $FIRE_PEAKS -u | sort-bed - > $ALL_TSS
 
@@ -206,7 +206,7 @@ for SNAME in "${SAMPLES[@]}"; do
     TSS_MINUS="tss_enrichment_intermediate/${SNAME}_gencodev45_Ensembl_canonical_TSS_FIRE_MINUS_Strand.bed"
     cat  $ALL_TSS | awk '$6 == "+"' > $TSS_PLUS
     cat  $ALL_TSS | awk '$6 == "-"' > $TSS_MINUS
-    SINGLE_BED="/mmfs1/gscratch/stergachislab/swansoe/projects/DddA/single_cell/expt1_HG002_FACS/msp_analysis/fibertools_msp/${SNAME}_consensus_BothStrands_HG38_corrected.haplotagged.MSP_BED6_single_150.bed.gz"
+    SINGLE_BED="../../msp_analysis/fibertools_msp/${SNAME}_consensus_BothStrands_HG38_corrected.haplotagged.MSP_BED6_single_150.bed.gz"
     PLUS_BED="tss_enrichment_intermediate/${SNAME}_DAF_tss_PLUS_2kb_MSPs_FIRE_peaks.bed"
     MINUS_BED="tss_enrichment_intermediate/${SNAME}_DAF_tss_MINUS_2kb_MSPs_FIRE_peaks.bed"
     PLUS_BG="tss_enrichment_intermediate/${SNAME}_DAF_tss_PLUS_2kb_MSPs_FIRE_peaks.bg"
@@ -223,27 +223,27 @@ done
 
 
 # scATAC-seq GM12878 --------------------------------------------------------
-FRAGMENTS=ATAC_REV2/fragments.tsv.gz
-SINGLE_BED=ATAC_REV2/passing_cell_fragments.tsv.gz
-WHITELIST=ATAC_REV2/10X_aggr_cell_barcodes.txt
+FRAGMENTS=../../data/fragments.tsv.gz
+SINGLE_BED=../../data/passing_cell_fragments.tsv.gz
+WHITELIST=../../data/10X_aggr_cell_barcodes.txt
 zcat $FRAGMENTS | grep -f $WHITELIST | sort-bed - | bgzip -@ 20 > $SINGLE_BED
 # TSS Enrichment Using ATAC-seq peaks
 SNAME="scATAC_GM12878"
-PEAKS="ATAC_REV2/10X_GM12878_aggr_peaks.bed"
-PEAKS_FILT="ATAC_REV2/10X_GM12878_aggr_peaks_FILT.bed"
+PEAKS="../../data/10X_GM12878_aggr_peaks.bed"
+PEAKS_FILT="../../data/10X_GM12878_aggr_peaks_FILT.bed"
 cat $PEAKS | grep -v KI | grep -v GL | grep -v "#" > $PEAKS_FILT
 # only include TSSs that are in peaks (active TSSs)
-ALL_TSS="ATAC_REV2/${SNAME}_gencodev45_Ensembl_canonical_TSS_ATAC_Peaks.bed"
+ALL_TSS="../../data/${SNAME}_gencodev45_Ensembl_canonical_TSS_ATAC_Peaks.bed"
 bedtools intersect -a $GENCODE -b $PEAKS_FILT -u | sort-bed - > $ALL_TSS
 # split by TSS strand
-TSS_PLUS="ATAC_REV2/${SNAME}_gencodev45_Ensembl_canonical_TSS_PLUS_Strand.bed"
-TSS_MINUS="ATAC_REV2/${SNAME}_gencodev45_Ensembl_canonical_TSS_MINUS_Strand.bed"
+TSS_PLUS="../../data/${SNAME}_gencodev45_Ensembl_canonical_TSS_PLUS_Strand.bed"
+TSS_MINUS="../../data/${SNAME}_gencodev45_Ensembl_canonical_TSS_MINUS_Strand.bed"
 cat  $ALL_TSS | awk '$6 == "+"' > $TSS_PLUS
 cat  $ALL_TSS | awk '$6 == "-"' > $TSS_MINUS
-PLUS_BED="ATAC_REV2/${SNAME}_tss_PLUS_2kb_MSPs_ATAC_peaks.bed"
-MINUS_BED="ATAC_REV2/${SNAME}_tss_MINUS_2kb_MSPs_ATAC_peaks.bed"
-PLUS_BG="ATAC_REV2/${SNAME}_tss_PLUS_2kb_MSPs_ATAC_peaks.bg"
-MINUS_BG="ATAC_REV2/${SNAME}_tss_MINUS_2kb_MSPs_ATAC_peaks.bg"
+PLUS_BED="../../data/${SNAME}_tss_PLUS_2kb_MSPs_ATAC_peaks.bed"
+MINUS_BED="../../data/${SNAME}_tss_MINUS_2kb_MSPs_ATAC_peaks.bed"
+PLUS_BG="../../data/${SNAME}_tss_PLUS_2kb_MSPs_ATAC_peaks.bg"
+MINUS_BG="../../data/${SNAME}_tss_MINUS_2kb_MSPs_ATAC_peaks.bg"
 bedops --ec --range 2000 --element-of 1 <(zcat $SINGLE_BED) $TSS_PLUS > $PLUS_BED
 bedtools genomecov -bg -i $PLUS_BED -g $SIZES > $PLUS_BG
 bedops --ec --range 2000 --element-of 1 <(zcat $SINGLE_BED) $TSS_MINUS > $MINUS_BED
@@ -251,33 +251,33 @@ bedtools genomecov -bg -i $MINUS_BED -g $SIZES > $MINUS_BG
 
 
 # ENCODE scATAC-seq (Pseudobulked) ------------------------------------
-FRAGS=$(ls ATAC_REV2/scATAC_*_fragments.tsv.gz)
+FRAGS=$(ls ../../data/scATAC_*_fragments.tsv.gz)
 for SINGLE_BED in $FRAGS; do
     BNAME=$(basename $SINGLE_BED | cut -d'_' -f2)
     SNAME="scATAC_${BNAME}"
-    PEAKS="ATAC_REV2/${SNAME}_peaks.narrowPeak"
-    PEAKS_FILT="ATAC_REV2/${SNAME}_peaks_FILT.narrowPeak"
+    PEAKS="../../data/${SNAME}_peaks.narrowPeak"
+    PEAKS_FILT="../../data/${SNAME}_peaks_FILT.narrowPeak"
     # Call Peaks
-    macs2 callpeak -f BED -t <(zcat $SINGLE_BED | sort-bed -) -g hs -n $SNAME --nomodel --shift -100 --extsize 200 --outdir ATAC_REV2
+    macs2 callpeak -f BED -t <(zcat $SINGLE_BED | sort-bed -) -g hs -n $SNAME --nomodel --shift -100 --extsize 200 --outdir ../../data
     cat $PEAKS | grep -v KI | grep -v GL | grep -v "#" > $PEAKS_FILT
 done
 
 for SINGLE_BED in $FRAGS; do
     BNAME=$(basename $SINGLE_BED | cut -d'_' -f2)
     SNAME="scATAC_${BNAME}"
-    PEAKS_FILT="ATAC_REV2/${SNAME}_peaks_FILT.narrowPeak"
+    PEAKS_FILT="../../data/${SNAME}_peaks_FILT.narrowPeak"
     # only include TSSs that are in peaks (active TSSs)
-    ALL_TSS="ATAC_REV2/${SNAME}_gencodev45_Ensembl_canonical_TSS_ATAC_Peaks.bed"
+    ALL_TSS="../../data/${SNAME}_gencodev45_Ensembl_canonical_TSS_ATAC_Peaks.bed"
     bedtools intersect -a $GENCODE -b $PEAKS_FILT -u | sort-bed - > $ALL_TSS
     # split by TSS strand
-    TSS_PLUS="ATAC_REV2/${SNAME}_gencodev45_Ensembl_canonical_TSS_PLUS_Strand.bed"
-    TSS_MINUS="ATAC_REV2/${SNAME}_gencodev45_Ensembl_canonical_TSS_MINUS_Strand.bed"
+    TSS_PLUS="../../data/${SNAME}_gencodev45_Ensembl_canonical_TSS_PLUS_Strand.bed"
+    TSS_MINUS="../../data/${SNAME}_gencodev45_Ensembl_canonical_TSS_MINUS_Strand.bed"
     cat  $ALL_TSS | awk '$6 == "+"' > $TSS_PLUS
     cat  $ALL_TSS | awk '$6 == "-"' > $TSS_MINUS
-    PLUS_BED="ATAC_REV2/${SNAME}_tss_PLUS_2kb_MSPs_ATAC_peaks.bed"
-    MINUS_BED="ATAC_REV2/${SNAME}_tss_MINUS_2kb_MSPs_ATAC_peaks.bed"
-    PLUS_BG="ATAC_REV2/${SNAME}_tss_PLUS_2kb_MSPs_ATAC_peaks.bg"
-    MINUS_BG="ATAC_REV2/${SNAME}_tss_MINUS_2kb_MSPs_ATAC_peaks.bg"
+    PLUS_BED="../../data/${SNAME}_tss_PLUS_2kb_MSPs_ATAC_peaks.bed"
+    MINUS_BED="../../data/${SNAME}_tss_MINUS_2kb_MSPs_ATAC_peaks.bed"
+    PLUS_BG="../../data/${SNAME}_tss_PLUS_2kb_MSPs_ATAC_peaks.bg"
+    MINUS_BG="../../data/${SNAME}_tss_MINUS_2kb_MSPs_ATAC_peaks.bg"
     bedops --ec --range 2000 --element-of 1 <(zcat $SINGLE_BED | sort-bed -) $TSS_PLUS > $PLUS_BED
     bedtools genomecov -bg -i $PLUS_BED -g $SIZES > $PLUS_BG
     bedops --ec --range 2000 --element-of 1 <(zcat $SINGLE_BED | sort-bed -) $TSS_MINUS > $MINUS_BED
@@ -286,7 +286,7 @@ done
 
 
 # OMNI-ATAC Bulk --------------------------
-FQ=$(ls ATAC_REV2/*OMNI-ATAC_*.fastq.gz)
+FQ=$(ls ../../data/*OMNI-ATAC_*.fastq.gz)
 for F in $FQ; do
     OMNI_BAM=${F/.fastq.gz/.bam};
     DEDUP=${OMNI_BAM/.bam/.DEDUP.bam};
@@ -299,28 +299,28 @@ for F in $FQ; do
     bedtools bamtobed -i $DEDUP | bgzip -@ 10 > $SINGLE_BED;
 done
 
-BAMS=$(ls ATAC_REV2/*OMNI-ATAC_*.DEDUP.bam)
+BAMS=$(ls ../../data/*OMNI-ATAC_*.DEDUP.bam)
 for B in $BAMS; do
     # TSS Enrichment Using ATAC-seq peaks
     SNAME=$(basename $B | cut -d'_' -f2)
-    PEAKS="ATAC_REV2/OMNI-ATAC_${SNAME}_peaks.narrowPeak"
-    PEAKS_FILT="ATAC_REV2/OMNI-ATAC_${SNAME}_peaks_FILT.narrowPeak"
+    PEAKS="../../data/OMNI-ATAC_${SNAME}_peaks.narrowPeak"
+    PEAKS_FILT="../../data/OMNI-ATAC_${SNAME}_peaks_FILT.narrowPeak"
     SINGLE_BED=${B/.DEDUP.bam/.bed.gz}
     # Call Peaks
-    macs2 callpeak -f BED -t <(zcat $SINGLE_BED | sort-bed -) -g hs -n "OMNI-ATAC_${SNAME}" --nomodel --shift -100 --extsize 200 --outdir ATAC_REV2
+    macs2 callpeak -f BED -t <(zcat $SINGLE_BED | sort-bed -) -g hs -n "OMNI-ATAC_${SNAME}" --nomodel --shift -100 --extsize 200 --outdir ../../data/
     cat $PEAKS | grep -v KI | grep -v GL | grep -v "#" > $PEAKS_FILT
     # only include TSSs that are in peaks (active TSSs)
-    ALL_TSS="ATAC_REV2/OMNI-ATAC_${SNAME}_gencodev45_Ensembl_canonical_TSS_ATAC_Peaks.bed"
+    ALL_TSS="../../data/OMNI-ATAC_${SNAME}_gencodev45_Ensembl_canonical_TSS_ATAC_Peaks.bed"
     bedtools intersect -a $GENCODE -b $PEAKS_FILT -u | sort-bed - > $ALL_TSS
     # split by TSS strand
-    TSS_PLUS="ATAC_REV2/OMNI-ATAC_${SNAME}_gencodev45_Ensembl_canonical_TSS_PLUS_Strand.bed"
-    TSS_MINUS="ATAC_REV2/OMNI-ATAC_${SNAME}_gencodev45_Ensembl_canonical_TSS_MINUS_Strand.bed"
+    TSS_PLUS="../../data/OMNI-ATAC_${SNAME}_gencodev45_Ensembl_canonical_TSS_PLUS_Strand.bed"
+    TSS_MINUS="../../data/OMNI-ATAC_${SNAME}_gencodev45_Ensembl_canonical_TSS_MINUS_Strand.bed"
     cat  $ALL_TSS | awk '$6 == "+"' > $TSS_PLUS
     cat  $ALL_TSS | awk '$6 == "-"' > $TSS_MINUS
-    PLUS_BED="ATAC_REV2/OMNI-ATAC_${SNAME}_tss_PLUS_2kb_MSPs_ATAC_peaks.bed"
-    MINUS_BED="ATAC_REV2/OMNI-ATAC_${SNAME}_tss_MINUS_2kb_MSPs_ATAC_peaks.bed"
-    PLUS_BG="ATAC_REV2/OMNI-ATAC_${SNAME}_tss_PLUS_2kb_MSPs_ATAC_peaks.bg"
-    MINUS_BG="ATAC_REV2/OMNI-ATAC_${SNAME}_tss_MINUS_2kb_MSPs_ATAC_peaks.bg"
+    PLUS_BED="../../data/OMNI-ATAC_${SNAME}_tss_PLUS_2kb_MSPs_ATAC_peaks.bed"
+    MINUS_BED="../../data/OMNI-ATAC_${SNAME}_tss_MINUS_2kb_MSPs_ATAC_peaks.bed"
+    PLUS_BG="../../data/OMNI-ATAC_${SNAME}_tss_PLUS_2kb_MSPs_ATAC_peaks.bg"
+    MINUS_BG="../../data/OMNI-ATAC_${SNAME}_tss_MINUS_2kb_MSPs_ATAC_peaks.bg"
     bedops --ec --range 2000 --element-of 1 <(zcat $SINGLE_BED | sort-bed -) $TSS_PLUS > $PLUS_BED
     bedtools genomecov -bg -i $PLUS_BED -g $SIZES > $PLUS_BG
     bedops --ec --range 2000 --element-of 1 <(zcat $SINGLE_BED | sort-bed -) $TSS_MINUS > $MINUS_BED
